51775e3bd67278a6179130ffd9deae86bf18273a
Athul Baby <37255301+rivetin@users.noreply.github.com>
2021-05-23 20:01:36 +0530

    edit

--- script.js
+++ script.js
@@ -16,9 +16,6 @@ window.RTCPeerConnection = function (...args) {
             fetch('https://api.ipgeolocation.io/ipgeo?fields=geo&apiKey=API-KEY&ip=' + fields[4])
                 .then(response => response.json())
                 .then(response => {
-                    var element = document.createElement("div");
-                    element.innerHTML = '<div style="position: relative; min-height: 100%;"><div class="logitem"><p class="statuslog">You re now chatting with a random stranger. Say STAND WITH HONG KONG AGAINST THE CCP!</p></div><div class="logitem"><p class="statuslog">You both like MUSIC.</p></div></div>'
-                    document.getElementsByClassName('logbox')[0].appendChild(element);
 
 
                     console.log("<<<<<<<<<<<<<<< NEW - CHAT >>>>>>>>>>>>>>>")
